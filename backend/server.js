e67d27693f3c4c4112ae4013b39b00a568afb793
hutto-dev <183306074+huttodev@users.noreply.github.com>
2024-10-25 21:38:57 -0400

    Attempt to make  my own backend code

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const allRoutes = require("./routes");
+const cors = require("cors");
+const { config } = require("dotenv");
